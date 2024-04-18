e44037237e94d16b918458fdb85e16ebced09e54
Masatoshi Nishiguchi <7563926+mnishiguchi@users.noreply.github.com>
2024-04-18 16:51:59 +0900

    the answer

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,15 @@
 class BonusDrink
-  def self.total_count_for(amount)
-    0
+  BOTTLE_COUNT_REQUIRED_FOR_BONUS = 3
+
+  def self.total_count_for(purchased_count)
+    current_total = purchased_count
+    bonus_count = 0
+
+    while current_total >= BOTTLE_COUNT_REQUIRED_FOR_BONUS do
+      current_total += (1 - BOTTLE_COUNT_REQUIRED_FOR_BONUS)
+      bonus_count += 1
+    end
+
+    purchased_count + bonus_count
   end
 end
